610887dc7cb8d896075d57f60c616c7457eecaf5
Nikola Muyot <nikola.muyot@gmail.com>
2024-03-12 23:02:36 -0400

    toggle mode

--- assets/js/form.js
+++ assets/js/form.js
@@ -56,4 +56,24 @@ document.addEventListener("DOMContentLoaded", function () {
     // // Redirect the user to the posts page (blog.html) after submission
     // window.location.href = "blog.html";
   });
+  // Access toggle switch HTML element
+  const themeSwitcher = document.querySelector("#theme-switcher");
+  const container = document.querySelector(".form-container");
+
+  // Set default mode to dark
+  let mode = "dark";
+
+  // Listen for a click event on toggle switch
+  themeSwitcher.addEventListener("click", function () {
+    // If mode is dark, apply light background
+    if (mode === "dark") {
+      mode = "light";
+      container.setAttribute("class", "light");
+    }
+    // If mode is light, apply dark background
+    else {
+      mode = "dark";
+      container.setAttribute("class", "dark");
+    }
+  });
 });
